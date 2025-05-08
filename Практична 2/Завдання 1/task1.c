45e8641b3e2c238d890c9ae6a53cfa96d710e570
Victoria Didychenko <143098745+tori-dn@users.noreply.github.com>
2025-05-08 20:46:34 +0300

    task1.c

--- Практична 2/Завдання 1/task1.c
+++ Практична 2/Завдання 1/task1.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <time. h>
+#include <limits.h>
 
+int main() {
+    time_t max_time = (time_t)~((time_t)1 << (sizeof (time_t) * 8 - 1));
+    printf ("Max time_t value: %ld\n", (long)max_time);
+    printf ("Date and time: %s", ctime(&max_time));
+    time_t overf low_time = max_time + 1;
+    printf ("After overf low: %s", ctime(&overflow_time) );
+
+    return 0;
+}
