3d4ad59ef2bfa89bc3c2e8c79f3dbb5b1390de0f
Victoria Didychenko <143098745+tori-dn@users.noreply.github.com>
2025-05-13 15:28:47 +0300

    Update task6.c

--- Практична 4 /Завдання 6/task6.c
+++ Практична 4 /Завдання 6/task6.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+    void *ptr = NULL;
+    size_t n = 32;
+
+    ptr = realloc(NULL, n);
+    if (ptr) {
+        printf("realloc(NULL, %zu) succeeded: %p\n", n, ptr);
+        free(ptr);
+    } else {
+        printf("realloc(NULL, %zu) failed\n", n);
+    }
+
+    ptr = malloc(n);
+    if (!ptr) {
+        perror("malloc");
+        return EXIT_FAILURE;
+    }
+
+    void *result = realloc(ptr, 0);
+    if (result == NULL) {
+        printf("realloc(ptr, 0) returned NULL (memory freed)\n");
+    } else {
+        printf("realloc(ptr, 0) returned non-NULL: %p (still freed)\n", result);
+        free(result); 
+    }
+
+    void *zero = realloc(NULL, 0);
+    if (zero == NULL) {
+        printf("realloc(NULL, 0) returned NULL\n");
+    } else {
+        printf("realloc(NULL, 0) returned non-NULL: %p\n", zero);
+        free(zero);
+    }
+
+    return 0;
+}
