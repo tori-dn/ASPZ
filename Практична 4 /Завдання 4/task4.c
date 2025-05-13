32786ed0e9041f61e47b92cdb48abdbf7bf0615a
Victoria Didychenko <143098745+tori-dn@users.noreply.github.com>
2025-05-13 16:23:12 +0300

    Update task4.c

--- Практична 4 /Завдання 4/task4.c
+++ Практична 4 /Завдання 4/task4.c
@@ -1,3 +1,5 @@
+
+// тестовий
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -9,19 +11,39 @@ int main() {
         if (!ptr)
             ptr = malloc(n);
 
+        ((char*)ptr)[0] = 'A'; 
+        ((char*)ptr)[1] = '\0';
+        printf("Iteration %d, ptr = %p, content = %s\n", i, ptr, (char*)ptr);
+
+        free(ptr);  
+    }
+
+    return 0;
+}
+
+// Правильний варіант:
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    size_t n = 16;
+
+    for (int i = 0; i < 3; ++i) {
+        void *ptr = malloc(n);  
+
         if (!ptr) {
             fprintf(stderr, "malloc failed\n");
             exit(EXIT_FAILURE);
         }
 
-        printf("Iteration %d, ptr = %p\n", i, ptr);
-        ((char*)ptr)[0] = 'A'; 
+        ((char*)ptr)[0] = 'A';
         ((char*)ptr)[1] = '\0';
-        printf("ptr content: %s\n", (char*)ptr);
 
-        free(ptr);
-        ptr = NULL; 
+        printf("Iteration %d, ptr = %p, content = %s\n", i, ptr, (char*)ptr);
+
+        free(ptr);  
     }
 
     return 0;
 }
+
