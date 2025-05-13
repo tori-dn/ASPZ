5a5e94fd6d7157895739db2edff9cd85a35e38b2
Victoria Didychenko <143098745+tori-dn@users.noreply.github.com>
2025-05-13 15:26:50 +0300

    Update task5.c

--- Практична 4 /Завдання 5/task5.c
+++ Практична 4 /Завдання 5/task5.c
@@ -1,1 +1,56 @@
+#task5.c
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    size_t big_size = (size_t)-1;  
+
+    void *ptr = malloc(1024);  
+    if (!ptr) {
+        perror("malloc");
+        return EXIT_FAILURE;
+    }
+
+    printf("Before realloc, ptr = %p\n", ptr);
+
+    ptr = realloc(ptr, big_size); 
+
+    if (!ptr) {
+        printf("realloc failed, memory leak occurred!\n");
+    } else {
+        printf("realloc succeeded, ptr = %p\n", ptr);
+        free(ptr);
+    }
+
+    return 0;
+}
+
+#task5_1.c
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    size_t big_size = (size_t)-1;
+
+    void *ptr = malloc(1024);
+    if (!ptr) {
+        perror("malloc");
+        return EXIT_FAILURE;
+    }
+
+    printf("Before realloc, ptr = %p\n", ptr);
+
+    void *tmp = realloc(ptr, big_size);
+    if (!tmp) {
+        printf("realloc failed, but old ptr is still valid: %p\n", ptr);
+        free(ptr);
+    } else {
+        ptr = tmp;
+        printf("realloc succeeded, ptr = %p\n", ptr);
+        free(ptr);
+    }
+
+    return 0;
+}
+
 
