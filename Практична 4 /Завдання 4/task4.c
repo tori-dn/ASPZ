e0712f235a2352afca40fc323bb74cd0f4a0375d
Victoria Didychenko <143098745+tori-dn@users.noreply.github.com>
2025-05-13 15:23:52 +0300

    Update task4.c

--- Практична 4 /Завдання 4/task4.c
+++ Практична 4 /Завдання 4/task4.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+    void *ptr = NULL;
+    size_t n = 16;
+
+    for (int i = 0; i < 3; ++i) {
+        if (!ptr)
+            ptr = malloc(n);
+
+        if (!ptr) {
+            fprintf(stderr, "malloc failed\n");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Iteration %d, ptr = %p\n", i, ptr);
+        ((char*)ptr)[0] = 'A'; 
+        ((char*)ptr)[1] = '\0';
+        printf("ptr content: %s\n", (char*)ptr);
+
+        free(ptr);
+        ptr = NULL; 
+    }
+
+    return 0;
+}
