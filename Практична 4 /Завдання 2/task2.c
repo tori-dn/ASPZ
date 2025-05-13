91b7ca08807ad60b8426e2f42cc16d681e17a39f
Victoria Didychenko <143098745+tori-dn@users.noreply.github.com>
2025-05-13 15:13:42 +0300

    task2.c

--- Практична 4 /Завдання 2/task2.c
+++ Практична 4 /Завдання 2/task2.c
@@ -1,1 +1,45 @@
+#task2_1.c
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
 
+int main() {
+    int xa = 1000000;
+    int xb = 1000000;
+    int num = xa * xb;
+    printf("num = %d (undefined behavior due to overflow)\n", num);
+
+    size_t size = num; 
+    printf("Pass a malloc(%zu)\n", size); 
+
+    void *ptr = malloc(size);
+    if (ptr == NULL) {
+        perror("malloc failed");
+    } else {
+        printf("Allocated %d bytes\n", num);
+        free(ptr);
+    }
+
+    return 0;
+}
+
+#task2_2.c
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    int negative_size = -1;
+    size_t size = negative_size; 
+
+    printf("Pass a malloc(%zu)\n", size); 
+
+    void *ptr = malloc(size);
+    if (ptr == NULL) {
+        perror("malloc failed");
+    } else {
+        printf("Allocated %zu bytes\n", size);
+        free(ptr);
+    }
+
+    return 0;
+}
