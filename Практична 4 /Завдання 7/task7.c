97c22947b3ade4b2324cf9d71a0e5d0a4da92c03
Victoria Didychenko <143098745+tori-dn@users.noreply.github.com>
2025-05-13 15:36:52 +0300

    Update task7.c

--- Практична 4 /Завдання 7/task7.c
+++ Практична 4 /Завдання 7/task7.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+struct sbar {
+  int data;
+};
+
+int main() {
+  struct sbar *ptr, *newptr;
+
+  ptr = calloc(1000, sizeof (struct sbar));
+  if (!ptr) {
+    perror ("calloc failed");
+    return 1;
+  }
+
+  printf ("calloc successful: ptr = %p\n", ptr);
+
+  newptr = reallocarray (ptr, 500, sizeof (struct sbar));
+  if (!newptr) {
+    perror ("reallocarray failed");
+    free (ptr);
+    return 1;
+  }
+
+  printf ("reallocarray successful: newptr = %p\n", newptr);
+
+  free (newptr);
+  return 0;
+}
