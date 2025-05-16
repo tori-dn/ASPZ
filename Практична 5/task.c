cea864e4eb19909300abe8d62928845483e5eaa4
Victoria Didychenko <143098745+tori-dn@users.noreply.github.com>
2025-05-16 22:08:21 +0300

    task.c

--- Практична 5/task.c
+++ Практична 5/task.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+void maybe_free_twice(int *p, int flag) {
+    free(p);
+    if (flag) {
+        free(p);
+    }
+}
+
+int main() {
+    int *ptr = malloc(sizeof(int));
+    *ptr = 100;
+
+    maybe_free_twice(ptr, 1); 
+
+    printf("Program has ended.\n");
+    return 0;
+}
